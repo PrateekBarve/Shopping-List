905a1eb50c7b683837aa1ae16ba8bb229fd8e5ed
Prateel Barve <prateekbarve2003@gmail.com>
2023-03-30 22:55:30 +0530

    initial commit

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,11 @@
+// Combining all the reducers here 
+
+import { combineReducers } from "redux";
+import { productReducer, selectedProductReducer } from "./productReducer";
+
+const reducers = combineReducers({
+    allProducts: productReducer, 
+    product: selectedProductReducer
+})
+
+export default reducers;
